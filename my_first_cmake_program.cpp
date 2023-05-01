4edd2e8726ccc8ca79aff82819540733a5492ed7
Ersyn <akhmetov.ersyn@gmail.com>
2023-05-01 16:14:39 +0300

    Добавьте файлы проекта.

---
+++ my_first_cmake_program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!";
+	return 0;
+}
